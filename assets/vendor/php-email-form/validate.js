054edf8c57a63d7667cd8c72c895c7de61965ef1
Xannydayo <yogaviveka1234@gmail.com>
2024-07-05 00:33:44 +0700

    fecthing validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -18,7 +18,6 @@
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
 
       let formData = new FormData( thisForm );
